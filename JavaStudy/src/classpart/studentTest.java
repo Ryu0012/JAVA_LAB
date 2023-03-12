d0d6d315466513d48b1bc78d9545eb10a8e8f444
seung <seung@LAPTOP-ryus.mshome.net>
2023-03-12 23:37:55 +0900

    05-4 인스턴스 여러 개 생성하기

--- JavaStudy/src/classpart/studentTest.java
+++ JavaStudy/src/classpart/studentTest.java
@@ -3,11 +3,13 @@ package classpart;
 public class studentTest {
 
 	public static void main(String[] args) {
-		Student studentAhn = new Student();
-		studentAhn.studentName = "안연수";
+		Student student1 = new Student();
+		student1.studentName = "안연수";
+		Student student2 = new Student();
+		student2.studentName = "안승연";
 		
-		System.out.println(studentAhn.studentName);
-		System.out.println(studentAhn.getStudentName());
+		System.out.println(student1.studentName);
+		System.out.println(student2.getStudentName());
 
 
 	}
